b9e4f057cc614de5b98ed022ac6cf7f28f509393
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2022-07-23 12:59:19 +0530

    removed all .gitignore files and folders

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 from setuptools import setup
 
+from pathlib import Path
+root_folder = Path(__file__).parent
+long_description = (root_folder / "README.md").read_text()
 
 setup(
     name='py-mrandom',
-    version='1.5.0',
+    version='0.1',
     description='mrandom is a library for generating random numbers.',
-    long_description=open('README.md').read(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Sidharth Mudgil',
     packages=['mrandom'],
     install_requires=[],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: Alpha',
         'Intended Audience :: Developers',
+        'Intended Audience :: Developers, Education, Students',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
@@ -19,5 +24,6 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ]
 )
